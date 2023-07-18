54d63f704aebaec35e011941a6b9fb32c9c7bb97
adam <adam@test.com>
2023-07-18 12:24:08 +0300

    Update website page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,58 +9,47 @@
           integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
           integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-            crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/cover/">
-    <link href="https://getbootstrap.com/docs/5.3/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <meta name="theme-color" content="#712cf9">
+    <style>
+        body {
+            text-shadow: 0 .05rem .1rem rgba(0, 0, 0, 0.9);
+            box-shadow: inset 0 0 5rem rgba(0, 0, 0, 0.1);
+            font-family: cursive;
+            font-style: italic;
+        }
 
-<style>
+        .nav-masthead .nav-link {
+            color: rgba(255, 255, 255, .5);
+            border-bottom: .25rem solid transparent;
+        }
 
-    body {
-        text-shadow: 0 .05rem .1rem rgba(0, 0, 0, 0.9);
-        box-shadow: inset 0 0 5rem rgba(0, 0, 0, 0.1);
-        font-family: cursive; font-style: italic;
-    }
+        .nav-masthead .nav-link:hover,
+        .nav-masthead .nav-link:focus {
+            border-bottom-color: rgba(255, 255, 255, .25);
+        }
 
-    .cover-container {
-        max-width: 42em;
-    }
+        .nav-masthead .nav-link + .nav-link {
+            margin-left: 1rem;
+        }
 
-    /*
-     * Header
-     */
+        .nav-masthead .active {
+            color: #fff;
+            border-bottom-color: #fff;
+        }
 
-    .nav-masthead .nav-link {
-        color: rgba(255, 255, 255, .5);
-        border-bottom: .25rem solid transparent;
-    }
+        .formError {
+            color: Red;
+        }
 
-    .nav-masthead .nav-link:hover,
-    .nav-masthead .nav-link:focus {
-        border-bottom-color: rgba(255, 255, 255, .25);
-    }
-
-    .nav-masthead .nav-link + .nav-link {
-        margin-left: 1rem;
-    }
-
-    .nav-masthead .active {
-        color: #fff;
-        border-bottom-color: #fff;
-    }
-
-</style>
+    </style>
 </head>
 
 <body class="d-flex h-100 text-center text-bg-dark">
 <div class="container d-flex w-100 mx-auto h-100 p-3 flex-column">
     <header class="mx-5">
         <div>
-            <h3 class="mb-0"><img src="{{ asset('fnd/logo.png') }}" height="50" width="50" > Fake News Detection
+            <h3 class="mb-0"><img src="{{ asset('fnd/logo.png') }}" height="50" width="50"> Fake News Detection
             </h3>
             <nav class="nav nav-masthead justify-content-center mt-2">
                 <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Home</a>
@@ -74,64 +63,71 @@
         <main class="px-3">
             <div>
                 <nav class="nav nav-masthead">
-                    <a id="text-link" class="nav-link fw-bold py-1 px-0 active" href="#" onclick="myFunction('text-test')">Author, Title and Text</a>
-                    <a id="url-link" class="nav-link fw-bold py-1 px-0" href="#" onclick="myFunction('url-test')">URL</a>
+                    <button id="text-link" class="btn btn-link nav-link fw-bold py-1 px-0 active"
+                       onclick="myFunction('text-test')">Author, Title and Text</button>
+                    <button id="url-link" class="btn btn-link nav-link fw-bold py-1 px-0"
+                       onclick="myFunction('url-test')">URL</button>
                 </nav>
             </div>
             <div id="text-test">
                 <div class="mt-5 text-left">
-                    <form method="post" action="{{ url('/text-test') }}">
+                    <form id="textForm" method="post" action="{{ url('/text-test') }}" enctype="multipart/form-data">
                         {{ csrf_field() }}
                         <div class="form-row">
                             <div class="form-group col-md-6">
                                 <h4>Title</h4>
-                                <input name="title" type="text" class="form-control" placeholder="Title">
+                                <input name="title" type="text" class="form-control" placeholder="Title" required>
                             </div>
                             <div class="form-group col-md-3">
                                 <h4>Author</h4>
-                                <input name="author" type="text" class="form-control" placeholder="Author">
+                                <input name="author" type="text" class="form-control" placeholder="Author" required>
                             </div>
                             <div class="form-group col-md-3">
                                 <h4>Source</h4>
-                                <input name="source" type="text" class="form-control" placeholder="Source">
+                                <input name="source" type="text" class="form-control" placeholder="Source" required>
                             </div>
                         </div>
                         <div class="form-group">
                             <div class="mb-3">
                                 <h4>News Text</h4>
-                                <textarea name="text" class="form-control" rows="3" placeholder="News Text ..."></textarea>
+                                <textarea name="text" class="form-control" rows="3"
+                                          placeholder="News Text ..." required></textarea>
                             </div>
                         </div>
                         <div class="text-right">
-                            <button type="submit" class="btn btn-primary">Check News</button>
+                            <button id="textCheckButton" type="button" class="btn btn-primary">Check News</button>
+                            <span id="textTimer" style="display: none;">Timer: <span class="Time">0</span>s</span>
                         </div>
                     </form>
                 </div>
-                <div class="mt-2 text-left">
+                <div class="mt-2 text-left formError">
+                    <span id="formError" style="display: none;"></span>
+                </div>
+                <div id="textResult" class="mt-2 text-left" style="display: none;">
                     <h5>Result</h5>
-                    <h6><a href="#">https://www.google.com</a> our probability 96%</h6>
-                    <h6><a href="#">https://www.youtube.com</a> our probability 82%</h6>
-                    <h6><a href="#">https://www.aljazeera.com</a> our probability 68%</h6>
+                    <div id="resultContent"></div>
                 </div>
             </div>
             <div id="url-test" style="display: none;">
                 <div class="mt-5 text-left">
-                    <form method="post" action="{{ url('/url-test') }}">
+                    <form id="urlForm" method="post" action="{{ url('/url-test') }}" enctype="multipart/form-data">
                         {{ csrf_field() }}
                         <div class="form-group">
                             <h4>URL</h4>
-                            <input type="text" class="form-control" placeholder="Url">
+                            <input type="text" class="form-control" placeholder="Url" required>
                         </div>
                         <div class="text-right">
-                            <button type="submit" class="btn btn-primary">Check News URL</button>
+                            <button id="urlCheckButton" type="button" class="btn btn-primary">Check News URL</button>
+                            <span id="urlTimer" style="display: none;">Timer: <span class="Time">0</span>s</span>
                         </div>
                     </form>
                 </div>
-                <div class="mt-2 text-left">
+                <div class="mt-2 text-left formError">
+                    <span id="formError" style="display: none;"></span>
+                </div>
+                <div id="urlResult" class="mt-2 text-left" style="display: none;">
                     <h5>Result</h5>
-                    <h6><a href="#">https://www.google.com</a> our probability 96%</h6>
-                    <h6><a href="#">https://www.youtube.com</a> our probability 82%</h6>
-                    <h6><a href="#">https://www.aljazeera.com</a> our probability 68%</h6>
+                    <div id="urlResultContent"></div>
                 </div>
             </div>
         </main>
@@ -144,21 +140,114 @@
 <script>
     function myFunction(id) {
         var x = document.getElementById(id);
-        if (id === "text-test"){
+        if (id === "text-test") {
             if (x.style.display === "none") {
                 x.style.display = "block";
             }
+            $(".formError").text("");
             document.getElementById('text-link').classList.add('active')
             document.getElementById('url-test').style.display = "none"
             document.getElementById('url-link').classList.remove('active')
-        }else if(id === "url-test"){
+        } else if (id === "url-test") {
             if (x.style.display === "none") {
                 x.style.display = "block";
             }
+            $(".formError").text("");
             document.getElementById('url-link').classList.add('active')
             document.getElementById('text-test').style.display = "none"
             document.getElementById('text-link').classList.remove('active')
         }
+    }
+
+    var timerInterval;
+
+    $(document).ready(function () {
+        $("#textCheckButton").click(function () {
+            var form = document.getElementById("textForm");
+            var formData = new FormData(form);
+            if (isFormValid(form)) {
+                $("#textCheckButton").prop("disabled", true);
+                $("#urlTimer").show();
+                startTimer();
+                $.ajax({
+                    url: form.action,
+                    method: "POST",
+                    data: formData,
+                    processData: false,
+                    contentType: false,
+                    success: function (response) {
+                        stopTimer();
+                        $("#textCheckButton").prop("disabled", false);
+                        $("#textResult").show();
+                        $("#resultContent").html(response);
+                    },
+                    error: function (error) {
+                        stopTimer();
+                        $("#textCheckButton").prop("disabled", false);
+                        $("#textResult").show();
+                        $("#resultContent").html("Error occurred during the AJAX request.");
+                    }
+                });
+                $(".formError").text("");
+            } else {
+                $(".formError").text("Please Fill All Required Inputs");
+            }
+        });
+
+        $("#urlCheckButton").click(function () {
+            var form = document.getElementById("urlForm");
+            var formData = new FormData(form);
+            if (isFormValid(form)) {
+                $("#urlCheckButton").prop("disabled", true);
+                $("#urlTimer").show();
+                startTimer();
+                $.ajax({
+                    url: form.action,
+                    method: "POST",
+                    data: formData,
+                    processData: false,
+                    contentType: false,
+                    success: function (response) {
+                        stopTimer();
+                        $("#urlCheckButton").prop("disabled", false);
+                        $("#urlResult").show();
+                        $("#urlResultContent").html(response);
+                    },
+                    error: function (error) {
+                        stopTimer();
+                        $("#urlCheckButton").prop("disabled", false);
+                        $("#urlResult").show();
+                        $("#urlResultContent").html("Error occurred during the AJAX request.");
+                    }
+                });
+                $(".formError").text("");
+            } else {
+                $(".formError").text("Please Fill All Required Inputs");
+            }
+        });
+    });
+
+    function isFormValid(form) {
+        var inputs = form.querySelectorAll("input, textarea");
+        for (var i = 0; i < inputs.length; i++) {
+            var input = inputs[i];
+            if (input.hasAttribute("required") && input.value.trim() === "") {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function startTimer() {
+        var time = 0;
+        timerInterval = setInterval(function () {
+            time++;
+            $(".Time").text(time);
+        }, 1000);
+    }
+
+    function stopTimer() {
+        clearInterval(timerInterval);
     }
 </script>
 </html>
