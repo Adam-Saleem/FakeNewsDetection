4b5b53cebfa91f1126a3248d001334fe25a16a78
adam <adam@test.com>
2023-07-18 19:10:18 +0300

    Update controller

--- app/Http/Controllers/FNDController.php
+++ app/Http/Controllers/FNDController.php
@@ -12,56 +12,48 @@ class FNDController extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 
+    public string $pythonPath;
+    public string $textTestPath;
+
+    public function __construct()
+    {
+        $this->pythonPath = public_path('python\venv\Scripts\python.exe');
+        $this->textTestPath = public_path('python\textTestPath.py');
+    }
 
     public function Text_Test(Request $request)
     {
         $title = $request->get('title');
-        $author =$request->get('author');
+        $author = $request->get('author');
         $source = $request->get('source');
         $text = $request->get('text');
 
+        $textFile = $this->createTempFile('text', $text);
+        $command = $this->pythonPath . ' ' . $this->textTestPath . ' ' . $title. ' ' . $author. ' ' . $source. ' ' . $textFile;
+        $result = shell_exec($command);
 
-        $pythonPath = public_path('python\venv\Scripts\python.exe');
-        $textRankOneTextPath = public_path('python\textRankOneText.py');
-        $normalizationPath = public_path('python\normalization.py');
-        $baysianPath = public_path('python\dynamicBaysian_Vectors.py');
-
-
-        $normalize_file_path = public_path('python\textNeedNormalize.txt');
-
-        file_put_contents($normalize_file_path, $title);
-        $normalizeTitleCommand = $pythonPath . ' ' . $normalizationPath . ' 2>&1';
-        $normalizeTitle = shell_exec($normalizeTitleCommand);
-
-        $text_file_path = public_path('python\text.txt');
-
-        file_put_contents($text_file_path,$text);
-        $textToSummary = $pythonPath . ' ' . $textRankOneTextPath . ' 2>&1';
-        $summary = shell_exec($textToSummary);
-
-        file_put_contents($normalize_file_path, $summary);
-        $summaryToNormalizeText = $pythonPath . ' ' . $normalizationPath . ' 2>&1';
-        $normalizeText = shell_exec($summaryToNormalizeText);
-
-        $normalizeTitle = str_replace(array("\r", "\n",",")," ",$normalizeTitle);
-        $normalizeText = str_replace(array("\r", "\n",",")," ",$normalizeText);
-        $author = str_replace(array("\r", "\n",",")," ",$author);
-        $source = str_replace(array("\r", "\n",",")," ",$source);
-
-        $testData_file_path = public_path('python\testData.csv');
-        $data = "title,summary,authors,source\n"."$normalizeTitle,$normalizeText,$author,$source";
-        file_put_contents($testData_file_path,$data);
-
-//        $baysianRun = $pythonPath . ' ' . $baysianPath . ' 2>&1';
-//        $result = shell_exec($baysianRun);
-//        logger($result);
-
-        return redirect('/');
+        return 'test summery title: ' . $result;
     }
 
     public function Url_Test(Request $request)
     {
         logger($request);
-        return redirect('/');
+        return 'url';
+    }
+
+    public function deleteTempFile($fileName)
+    {
+        $filePath = public_path('temp') . DIRECTORY_SEPARATOR . $fileName;
+        if (file_exists($filePath)) {
+            unlink($filePath);
+        }
+    }
+
+    public function createTempFile($fileName, $content): string
+    {
+        $fileName = $fileName . '_' . uniqid() . '.txt';
+        $filePath = public_path('temp') . DIRECTORY_SEPARATOR . $fileName;
+        file_put_contents($filePath, $content);
+        return $fileName;
     }
 }
